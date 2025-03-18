6752c8f1b34e8b56ab20cca5c3f8c62503dd35c4
EdivanCarvalho1 <edivanjuniorfreitas@gmail.com>
2025-03-17 21:40:09 -0300

    TR07-CustomQueryRepository

--- src/main/java/br/edu/iff/ccc/bsi/forumhub/repository/PersonRepository.java
+++ src/main/java/br/edu/iff/ccc/bsi/forumhub/repository/PersonRepository.java
@@ -3,8 +3,16 @@ package br.edu.iff.ccc.bsi.forumhub.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.Qu
+
+import br.edu.iff.ccc.bsi.forumhub.enums.STATUS;
+
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import br.edu.iff.ccc.bsi.forumhub.enums.STATUS;
 import br.edu.iff.ccc.bsi.forumhub.model.Person;
 
 @Repository
@@ -13,4 +21,8 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
 	public List<Person> findByNicknameAndPhone(String nickname, String phone);
 	
 	public Person findByNickname(String nickname);
+	
+
+    @Query("SELECT p FROM Person p WHERE p.status = :status ORDER BY p.points DESC")
+    List<Person> findActivePersonsOrderedByPoints(@Param("status") STATUS status);
 }
